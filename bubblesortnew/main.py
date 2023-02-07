48bb6b374ebce681971b305a0316760688d4dcd0
manasjuneja <junejamans@proton.me>
2023-02-07 09:57:32 +0530

    initial commit for bubble sort new

---
+++ bubblesortnew/main.py
@@ -0,0 +1,14 @@
+def bubblesortnew(list):
+
+    for i in range(len(list)-1):
+
+        for j in range(len(list)-i-1):
+
+            if list[j] > list[j+1]:
+
+                temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+
+    return list
+
