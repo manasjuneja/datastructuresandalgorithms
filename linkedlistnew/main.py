a34089a9c362de0f8e8a083212554697c3ea9fee
manasjuneja <junejamans@proton.me>
2023-02-27 22:25:51 +0530

    initial commit

--- linkedlistnew/main.py
+++ linkedlistnew/main.py
@@ -4,21 +4,3 @@ class Node:
         self.next = None
 
 
-def getlength(node):
-    count = 1
-    cur = node
-
-    while cur.next != None :
-        cur = cur.next
-        count += 1
-
-    return count 
-
-node_1 = Node(32)
-node_2 = Node(89)
-node_1.next = node_2
-print(getlength(node_1))
-
-
-
-# def findindex(node, value):
