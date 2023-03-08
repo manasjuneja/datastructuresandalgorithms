7992746c2aed3be8e254251c52d1674eb74120b0
manasjuneja <junejamans@proton.me>
2023-03-08 23:52:47 +0530

    added getlength feature

--- linkedlistnew/main.py
+++ linkedlistnew/main.py
@@ -4,3 +4,14 @@ class Node:
         self.next = None
 
 
+def getlength(node):
+    cur = node
+
+    count = 1
+
+    while cur.next != None :
+        cur = cur.next
+        count += 1
+
+    return count
+
